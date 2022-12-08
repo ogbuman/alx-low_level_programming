2eccf84ac8f43194194c8de21c6f19c7c1f14f82
ogbuman <ogbuman@gamil.com>
2022-12-08 11:35:03 -0800

    low_level

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,4 @@
 #include <stdio.h>
-/**
- * main - prints this phrase Programming is like
- * building a Multilingual puzzle.
- *
- * Return: 0 if exited properly, non-zero
- */
-int main(void){puts("\"Programming is like building a multilingual puzzle"); return (0);}
+int main(void){
+  puts("\"Programming is like building a multilingual puzzle");
+  return (0);}
