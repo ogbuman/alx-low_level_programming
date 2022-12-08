5898a5b5a5e994cdcb65a799485a318d4bd0aa6c
ogbuman <ogbuman@gamil.com>
2022-12-08 13:56:16 -0800

    first c code

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ *main - this is the entry point
+ *Return: this is the return value 0
+ */
+int main(void)
+{
+puts("\"programming is like building a multilingual puzzle");
+return (0);
+}
