124dc1129b4015ebd533e3b257f998e8830a8f47
ogbuman <ogbuman@gamil.com>
2022-12-08 11:30:05 -0800

    low_level

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,8 +5,4 @@
  *
  * Return: 0 if exited properly, non-zero
  */
-int main(void)
-{
-  puts("\"Programming is like building a multilingual puzzle"); 
-  return (0);
-}
+int main(void){puts("\"Programming is like building a multilingual puzzle"); return (0);}
