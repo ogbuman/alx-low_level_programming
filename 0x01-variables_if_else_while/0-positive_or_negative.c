48c7754078810b9294a97d8ce90485e052645b17
ogbuman <ogbuman@gamil.com>
2022-12-09 10:32:01 -0800

    updated

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -15,8 +15,8 @@ n = rand() - RAND_MAX / 2;
 if (n > 0)
 printf("%d is positive", n);
 else if (n == 0)
-prinf("%d is zero", n);
+printf("%d is zero", n);
 else if (n < 0)
-prinf("%d is negative", n);
+printf("%d is negative", n);
 return (0);
 }
