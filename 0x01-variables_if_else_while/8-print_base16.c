6ae1b9c42294949bcd1d16dd8d62d9abc138ea66
ogbuman <ogbuman@gamil.com>
2022-12-11 13:38:35 -0800

    update

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+* main - prints the lowercase alphabet in reverse,
+* followed by a new line
+* Return: 0 always succes
+*/
+int main(void)
+{
+int n;
+char ch;
+for (n = '0'; n <= '9'; n++)
+{
+putchar(n);
+}
+for (ch = 'a'; ch <= 'f'; ch++)
+{
+putchar(ch);
+}
+putchar(10);
+return (0);
+}
