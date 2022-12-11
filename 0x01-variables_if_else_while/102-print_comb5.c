16fe3489b553d32ffbe3c1474aafb7a4976b2549
ogbuman <ogbuman@gamil.com>
2022-12-11 15:24:24 -0800

    update

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,67 +1,31 @@
 #include <stdio.h>
-
-
-
 /**
-
- * main - prints all possible comb of two
-
- * digits number
-
- * Return: 0 to show the succes of the program
-
- */
-
+* main - prints all possible comb of two
+* digits number
+* Return: 0 to show the succes of the program
+*/
 int main(void)
-  
 {
-  
-  int n, m;
-  
-
-  
-  for (n = 0; n < 100; n++)
-    
-    {
-      
-      for (m = 0; m < 100; m++)
-	
-	{
-	  
-	  if (n < m)
-	    
-	    {
-	      
-	      putchar((n / 10) + 48);
-	      
-	      putchar((n % 10) + 48);
-	      
-	      putchar(' ');
-	      
-	      putchar((m / 10) + 48);
-	      
-	      putchar((m % 10) + 48);
-	      
-	      if (n != 98 || m != 99)
-		
-		{
-		  
-		  putchar(',');
-		  
-		  putchar(' ');
-		  
-		}
-	      
-	    }
-	  
-	}
-      
-    }
-  
-  putchar('\n');
-  
-  return (0);
-
+int n, m;
+for (n = 0; n < 100; n++)
+{
+for (m = 0; m < 100; m++)
+{
+if (n < m)
+{
+putchar((n / 10) + 48);
+putchar((n % 10) + 48);
+putchar(' ');
+putchar((m / 10) + 48);
+putchar((m % 10) + 48);
+if (n != 98 || m != 99)
+{
+putchar(',');
+putchar(' ');
 }
-
-
+}
+}
+}
+putchar('\n');
+return (0);
+}
