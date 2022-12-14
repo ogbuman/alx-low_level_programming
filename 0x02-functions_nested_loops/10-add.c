306faaa6811b53fb1076e56929d79bdf35bb8b84
ogbuman <ogbuman@gamil.com>
2022-12-14 13:34:30 -0800

    update

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,27 +1,12 @@
 #include "main.h"
-
-
-
 /**
-
- * add - Add two integers
-
- * @x: int type number
-
- * @y: int type number
-
- * Return: result of addition
-
- */
-
+* add - Add two integers
+* @x: int type number
+* @y: int type number
+* Return: result of addition
+*/
 int add(int x, int y)
-  
 {
-  
-  int solve = (x + y);
-  
-
-  
-  return (solve);
-  
+int solve = (x + y);
+return (solve);
 }
