95b37b0479993799dfc685fcd1b0417693be3b49
ogbuman <ogbuman@gamil.com>
2022-12-14 13:31:45 -0800

    update

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,87 +1,35 @@
 #include "main.h"
-
 /**
-
- * times_table - Print the 9 times table, starting with 0
-
- * Return: 0
-
- */
-
+* times_table - prints the 9 times table
+* starting with 0
+*/
 void times_table(void)
-  
 {
-  
-  int row = 0; /* rows */
-  
-  int col;     /* columns*/
-  
-  int n;     /* values*/
-  
-
-  
-  while (row < 10) /*calculate values for each rows*/
-    
-    {
-      
-      col = 0;
-      
-
-      
-      while (col < 10)
-	
-	{
-	  
-	  n = row * col; /*multiplies each rows by columns to get n*/
-	  
-
-	  
-	  if (n > 9)
-	    
-	    {
-	      
-	      _putchar(n / 10 + '0');
-	      
-	      _putchar(n % 10 + '0');
-	      
-	    }
-	  
-	  else if (col != 0) /*if column not the first print space*/
-	    
-	    {
-	      
-	      _putchar(' ');
-	      
-	      _putchar(n + '0');
-	      
-	    }
-	  
-	  else
-	    
-	    {
-	      
-	      _putchar(n + '0');
-	      
-	    }
-	  
-	  if (col != 9)
-	    
-	    {
-	      
-	      _putchar(',');
-	      
-	      _putchar(' ');
-	      
-	    }
-	  
-	  col++; /*increment column*/
-	  
-	}
-      
-      _putchar('\n');
-      
-      row++;
-      
-    }
-  
+int i, j, k;
+for (i = 0; i < 10; i++)
+{
+for (j = 0; j < 10; j++)
+{
+k = j * i;
+if (j == 0)
+{
+_putchar(k + '0');
+}
+if (k < 10 && j != 0)
+{
+_putchar(',');
+_putchar(' ');
+_putchar(' ');
+_putchar(k + '0');
+}
+else if (k >= 10)
+{
+_putchar(',');
+_putchar(' ');
+_putchar((k / 10) + '0');
+_putchar((k % 10) + '0');
+}
+}
+_putchar('\n');
+}
 }
