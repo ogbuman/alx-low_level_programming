c1ef62367e6d6c55e501d4156e3e343e4e1c8e98
ogbuman <ogbuman@gamil.com>
2022-12-14 11:42:05 -0800

    update

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,25 +1,13 @@
 #include "main.h"
-
 /**
-
- * _abs - prints the absolute value of an integer
-
- * @n: int type number
-
- * Return: absolute value of @n if negative or otherwise
-
- */
-
+* _abs - prints the absolute value of an integer
+* @n: int type number
+* Return: absolute value of @n if negative or otherwise
+*/
 int _abs(int n)
-  
 {
-  
-  if (n < 0)
-    
-    return (n * -1);
-  
-  else
-    
-    return (n);
-  
+if (n < 0)
+return (n * -1);
+else
+return (n);
 }
