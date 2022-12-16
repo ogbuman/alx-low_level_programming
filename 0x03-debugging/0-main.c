320c0d2ce0e45a8f1a0beb51ba5e52eb7f38b20e
ogbuman <ogbuman@gamil.com>
2022-12-15 19:55:54 -0800

    updated

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
-* main - testing the positive or negative func
-* Return: All good
+* main - tests function that prints if integer is positive or negative
+* Return: 0
 */
 int main(void)
 {
