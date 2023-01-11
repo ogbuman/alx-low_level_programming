a915e85a6b3e55a9f553ea26bcf036b79eee9f39
ogbuman <ogbuman@gamil.com>
2023-01-10 16:15:09 -0800

    updated

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -14,8 +14,10 @@ if (s1 == NULL)
 s1 = "";
 if (s2 == NULL)
 s2 = "";
-for (i = 0; s1[i] != '\0'; i++);
-for (j = 0; s2[j] != '\0'; j++);
+for (i = 0; s1[i] != '\0'; i++)
+;
+for (j = 0; s2[j] != '\0'; j++)
+;
 a = malloc((i * sizeof(char)) + (j * sizeof(char)) + 1);
 if (a == NULL)
 return (NULL);
