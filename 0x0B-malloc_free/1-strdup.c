7598f84668969704d001abcf06ad9019b257397d
ogbuman <ogbuman@gamil.com>
2023-01-10 16:03:15 -0800

    updated

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -11,7 +11,7 @@ char *a;
 int i, j;
 if (str == NULL)
 return (NULL);
-for (i = 0; str[i] != '\0'; i++);
+for (i = 0; str[i] != '\0'; i++)
 a = malloc(i * sizeof(char) + 1);
 if (a == NULL)
 return (NULL);
