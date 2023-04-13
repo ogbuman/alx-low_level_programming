fe6d37aec157128f3b83ea2aacb9c4e68417d630
ogbuman <ogbuman@gamil.com>
2023-04-12 18:26:54 -0700

    hash1

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,27 +1,31 @@
-#include hash_tables.h
+#include "hash_tables.h"
 
 /**
- * hash_table_create - Creates a hash table.
- * @size: The size of the array.
- *
- * Return: If an error occurs - NULL.
- *         Otherwise - a pointer to the new hash table.
- */
+ * hash_table_create - creates a hash table
+ * @size: size of array
+ * Return: hashtable or NULL if failed
+*/
 hash_table_t *hash_table_create(unsigned long int size)
 {
-README.md hash_table_t *ht;
-README.md unsigned long int i;
+hash_table_t *hashtable;
+unsigned int i;
 
-README.md ht = malloc(sizeof(hash_table_t));
-README.md if (ht == NULL)
-README.md README.md return (NULL);
+if (size == 0)
+return (NULL);
+hashtable = malloc(sizeof(hash_table_t));
+if (hashtable == NULL)
+return (NULL);
+hashtable->array = malloc(sizeof(hash_node_t *) * size);
+if (hashtable->array == NULL)
+{
+free(hashtable);
+return (NULL);
+}
+hashtable->size = size;
 
-README.md ht->size = size;
-README.md ht->array = malloc(sizeof(hash_node_t *) * size);
-README.md if (ht->array == NULL)
-README.md README.md return (NULL);
-README.md for (i = 0; i < size; i++)
-README.md README.md ht->array[i] = NULL;
-
-README.md return (ht);
+for (i = 0; i < size; i++)
+{
+hashtable->array[i] = NULL;
+}
+return (hashtable);
 }
